3792683585e04872d48835563ad89e509ece22ad
Darum <waxabbac@gmail.com>
2025-05-19 03:07:35 +0400

    upd  Changes to be committed: 	modified:   "680.22 Аббасов Мурад.docx" 	new file:   aboutus_page/aboutus.css 	new file:   aboutus_page/aboutus.html 	new file:   aboutus_page/languages/en.js 	modified:   main_page/main.html 	modified:   mensclothing_page/mensclothing.html 	modified:   womensclothing_page/womensclothing.html

---
+++ aboutus_page/languages/en.js
@@ -0,0 +1,10 @@
+var translation_en = {
+    "langcur": "Language",
+    "topbanner1": "Cheap and cheerful",
+    "aboutus": "About us",
+    "aboutusdesc": "Welcome to Sport Sort! We are your reliable partner in the world of sportswear. Our mission is to provide high-quality, stylish and affordable clothing for an active lifestyle. Our company strives to support athletes and fitness enthusiasts by offering a wide range of products.",
+    "aboutusdesc2": "We pride ourselves on our attention to detail, eco-friendly manufacturing and excellent customer service. Join us and discover a world of comfort and style!",
+    "mail": "E-mail",
+    "location": "Location",
+    "allrights": "all rights reserved"
+};
